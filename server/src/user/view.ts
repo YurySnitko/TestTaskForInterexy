7af03e0b03f17146ab835a4a7378a8ba6881663c
YurySnitko <yury.snitko@gmail.com>
2022-12-01 16:08:33 +0300

    basic server setup implemented

---
+++ server/src/user/view.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import controller from "./controller";
+
+export const signup = async (req: Request, res: Response) => {
+  const user = await controller.signup(req.body);
+
+  res.json({ user });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const token = await controller.login(req.body.email, req.body.password);
+
+  res.json({ token });
+};
+
+export const me = async (req: any, res: Response) => {
+  const profile = await controller.read(req.user._id);
+
+  res.json({ profile });
+};
